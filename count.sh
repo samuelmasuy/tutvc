376493df3625d92548bc453fe1bc1c498395f03e
Samuel Masuy <samuel.masuy@gmail.com>
2014-09-27 12:14:38 -0400

    Importing files from sheell tutorial

---
+++ count.sh
@@ -0,0 +1,7 @@
+# Display the name of each data file and count of the number
+# of distinct occurences of each species in that file.
+for file_name in data/*.txt
+do
+    echo $file_name
+    grep -v Species $file_name | cut -d , -f 2 | sort | uniq -c
+done
