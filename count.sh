0c67bd8a88ff743c0272032de72e7e44c5a114ba
Samuel Masuy <samuel.masuy@gmail.com>
2014-09-27 12:22:49 -0400

    Removed the comments

--- count.sh
+++ count.sh
@@ -1,6 +1,5 @@
 # Display the name of each data file and count of the number
 # of distinct occurences of each species in that file.
-# Results are displayed in alphabetical order by species name.
 for file_name in data/*.txt
 do
     echo $file_name
